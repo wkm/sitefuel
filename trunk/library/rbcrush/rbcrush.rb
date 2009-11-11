d2a922f31c335010956aa02213d9bf78e449730d
wkm <wkm@zanoccio.com>
2009-11-11 00:50:54 +0000

    -- added a library/ directory for sitefuel specific libraries  -- added the start of a rbcrush wrapper library that abstracts away pngcrush
    git-svn-id: file:///svn/sitefuel@4 a5b15aab-b829-47cf-af43-c6be6f7dd6fb

---
+++ trunk/library/rbcrush/rbcrush.rb
@@ -0,0 +1,16 @@
+#
+# File::      rbcrush.rb
+# Author::    wkm
+# Copyright:: 2009
+# License::   GPL
+#
+# A ruby wrapper around the pngcrush program.
+#
+
+module Rbcrush
+  class Rbcrush
+    def self.process(filename)
+      
+    end
+  end
+end
