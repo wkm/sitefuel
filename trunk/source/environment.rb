39298980ce2fdb708547934c179065cc39dcde0f
wkm <wkm@zanoccio.com>
2009-11-14 06:09:53 +0000

    Added a package with all the gems required as well their version specifications.
    git-svn-id: file:///svn/sitefuel@7 a5b15aab-b829-47cf-af43-c6be6f7dd6fb

---
+++ trunk/source/environment.rb
@@ -0,0 +1,12 @@
+#
+# File::      environment.rb
+# Author::    wkm
+# Copyright:: 2009
+# License::   GPL
+#
+
+require 'rubygems'
+
+gem 'hpricot',   '~> 0.8'   # html parsing
+gem 'jsmin',     '~> 1.0'   # javascript minify
+gem 'cssmin',    '~> 1.0'   # css minify
