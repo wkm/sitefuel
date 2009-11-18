78d956c39601fecfdd60df10cd211fde7a95e095
wkm <wkm@zanoccio.com>
2009-11-18 04:58:13 +0000

    better example of String#abbrev
    git-svn-id: file:///svn/sitefuel@33 a5b15aab-b829-47cf-af43-c6be6f7dd6fb

--- trunk/lib/mixins/StringFormatting.rb
+++ trunk/lib/mixins/StringFormatting.rb
@@ -7,7 +7,10 @@
 
 class String
 
-  # gives an abbreviated form of a string
+  # gives an abbreviated form of a string, showing some of the beginning
+  # and some of the end.
+  # 
+  #  "the quick brown dog".abbrev 12  # => "the q... dog"
   def abbrev(len)
     if length <= len
       self
@@ -15,4 +18,6 @@ class String
       self[0..(len/2-2).floor] + "..." + self[(length - len/2+2) .. (length)]
     end
   end
+
+  p "the quick brown dog".abbrev 12
 end
