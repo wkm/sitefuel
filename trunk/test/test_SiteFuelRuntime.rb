d48498d9907c532bc61ecd31e4b7645c06af618c
wkm <wkm@zanoccio.com>
2009-11-21 10:37:35 -0600

    created unit tests for the runtime
    Finally created a TestSiteFuelRuntime class. Currently contains tests involving finding processors and choosing which processor to run based on file name.

---
+++ trunk/test/test_SiteFuelRuntime.rb
@@ -0,0 +1,68 @@
+#
+# File::        test_SiteFuelRuntime.m
+# Author::      wkm
+# Copyright::   2009
+# License::     GPL
+#
+# Unit tests for the SiteFuelRuntime class.
+#
+
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'test/unit'
+require 'SiteFuelRuntime.rb'
+
+include SiteFuel
+
+class HTMLClasherTest < Processor::AbstractProcessor
+  def self.file_patterns
+    [".html"]
+  end
+end
+
+class TestSiteFuelRuntime < Test::Unit::TestCase
+
+  def setup
+    @runtime = SiteFuelRuntime.new
+  end
+
+  def test_processor_picking
+    assert_equal Processor::CSSProcessor, @runtime.choose_processor("foo.css")
+
+    assert_equal Processor::HTMLProcessor, @runtime.choose_processor("foo.html")
+    assert_equal Processor::HTMLProcessor, @runtime.choose_processor("FOO.HtM")
+
+    assert_nil @runtime.choose_processor("foo.xxxx")
+    assert_nil @runtime.choose_processor("foocss")
+    assert_nil @runtime.choose_processor("foohtml")
+    p dawg
+  end
+
+  def test_processor_finding
+    # test that we don't have the clasher, since it doesn't end with Processor
+    assert !SiteFuelRuntime.find_processors.include?(HTMLClasherTest), 'HTMLClasherTest included'
+
+    # test that we do have the basic processor test suite
+    assert_equal(
+      SiteFuelRuntime.find_processors.sort,
+      [
+        Processor::HTMLProcessor,
+        Processor::CSSProcessor,
+        Processor::SassProcessor
+      ].sort
+    )
+  end
+
+  def test_processor_clashing
+    @runtime.add_processor(HTMLClasherTest)
+    
+    assert_raise Processor::MultipleApplicableProcessors do
+      @runtime.choose_processor("foo.html")
+    end
+
+    assert_nothing_raised @runtime.choose_processor!("foo.html")
+
+    assert_equal Processor::HTMLProcessor, @runtime.choose_processor("foo.htm")
+
+  end
+end
