54b085d16b5f9ac56492c8fde3530735499459d2
wkm <wkm@zanoccio.com>
2009-11-24 22:07:43 -0600

    Oopsie. Refactored String.abbrev ==> String.cabbrev in this file as well.

--- test/test_StringFormatting.rb
+++ test/test_StringFormatting.rb
@@ -5,11 +5,11 @@ require 'mixins/StringFormatting'
 
 class TestString < Test::Unit::TestCase
   def test_abbrev
-    assert_equal "hello world", "hello world".abbrev(11)
-    assert_equal "hello world", "hello world".abbrev(12)
+    assert_equal "hello world", "hello world".cabbrev(11)
+    assert_equal "hello world", "hello world".cabbrev(12)
 
-    assert_equal 12, "the quick brown fox jumped over the lazy dog".abbrev(12).length
-    assert_equal "the q... dog", "the quick brown fox jumped over the lazy dog".abbrev(12)
-    assert_equal "the quick brown fox... over the lazy dog", "the quick brown fox jumped over the lazy dog".abbrev(40)
+    assert_equal 12, "the quick brown fox jumped over the lazy dog".cabbrev(12).length
+    assert_equal "the q... dog", "the quick brown fox jumped over the lazy dog".cabbrev(12)
+    assert_equal "the quick brown fox... over the lazy dog", "the quick brown fox jumped over the lazy dog".cabbrev(40)
   end
 end
