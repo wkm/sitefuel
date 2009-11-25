34af54ecef96ac05540367a66733640ec6432618
wkm <wkm@zanoccio.com>
2009-11-24 19:10:34 -0600

    Finally paid some attention to the logging.
    Made SiteFuelLogger:: a singleton class.
    
    Added SiteFuelLogger#fatal_count, #error_count, #warning_count, #info_count, and #debug_count. These track the number of messages of the given type have been logged.

--- lib/SiteFuelLogger.rb
+++ lib/SiteFuelLogger.rb
@@ -7,24 +7,86 @@
 
 module SiteFuel
   require 'Logger'
+  require 'singleton'
 
-  # Abstraction around the Logger:: library, typically every processor
+  # Singleton abstraction around the Logger:: library, typically every processor
   # will hook into using this logger while letting us control it's placement
   # and display globally
-  class SiteFuelLogger
-    attr_reader :log
+  class SiteFuelLogger < Logger
+    include Singleton
 
-    def initialize(filename, archive_size)
-      @log = Logger.new(filename)
+    # the number of fatal messages logged so far (even if lower than #level)
+    attr_reader :fatal_count
+
+    # the number of error messages logged so far (even if lower than #level)
+    attr_reader :error_count
+
+    # the number of warning messages logged so far (even if lower than #level)
+    attr_reader :warning_count
+
+    # the number of info messages logged so far (even if lower than #level)
+    attr_reader :info_count
+
+    # the number of debug messages logged so far (even if lower than #level)
+    attr_reader :debug_count
+
+    def initialize(filename = STDOUT)
+      @fatal_count = 0
+      @error_count = 0
+      @warning_count = 0
+      @info_count = 0
+      @debug_count = 0
+      
+      super(filename)
     end
 
-    def level=
+    def fatal(*args)
+      @fatal_count += 1
+      super(*args)
     end
 
-    # TODO: should we just inherit Logger?
-    def error(*args) @log.error(*args); end
-    def warn(*args) @log.warn(*args); end
-    def info(*args) @log.info(*args); end
-    def debug(*args) @log.debug(*args); end
+    def error(*args)
+      @error_count += 1
+      super(*args)
+    end
+
+    def warn(*args)
+      @warning_count += 1
+      super(*args)
+    end
+
+    def info(*args)
+      @info_count += 1
+      super(*args)
+    end
+
+    def debug(*args)
+      @debug_count += 1
+      super(*args)
+    end
+  end
+
+  # mixin for adding logging functionality to any class, typically included
+  # by every SiteFuel class
+  module Logging
+    # sets the logger for a class
+    def logger=(logger)
+      @logger = logger
+    end
+
+    # adds a fatal error to the log
+    def fatal(*args) @log.fatal(*args); end
+    
+    # adds an error to the log
+    def error(*args) @logger.error(*args); end
+
+    # adds a warning to the log
+    def warn(*args) @logger.warn(*args); end
+
+    # adds an info message to the log
+    def info(*args) @logger.info(*args); end
+
+    # adds a debugging message to the log
+    def debug(*args) @logger.debug(*args); end
   end
 end
