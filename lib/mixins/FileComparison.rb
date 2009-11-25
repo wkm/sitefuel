fb6a5a7649697b871a8633c89648ef9bfa7311e8
wkm <wkm@zanoccio.com>
2009-11-24 22:05:18 -0600

    Committed the actual File.equivalent? method.
    It works by

--- lib/mixins/FileComparison.rb
+++ lib/mixins/FileComparison.rb
@@ -7,10 +7,18 @@
 # Adds File.equivalent? that tries to guess whether two files are equivalent
 #
 
+require 'mixins/ArrayComparisons'
+
 class File
+  # gives true if one of the paths is the ending of another path
+  #
+  #  File.equivalent?('b/a.rb', 'a.rb')  # ==> true
+  #  File.equivalent?('c/b/a.rb', 'c/a.rb')  # ==> false
   def self.equivalent?(a, b)
-    fa = File.expand_path(a)
-    fb = File.expand_path(b)
-    p fa, fb
+    list = [
+      File.expand_path(a, '/').split(File::SEPARATOR)[1 .. -1],
+      File.expand_path(b, '/').split(File::SEPARATOR)[1 .. -1]
+    ].sort {|l,r| l.length <=> r.length }.reverse
+    list.last.ends_with? list.first
   end
 end
