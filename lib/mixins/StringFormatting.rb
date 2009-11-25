47ef85df7163b0b82d528c0d540e2668aa98e2e7
wkm <wkm@zanoccio.com>
2009-11-24 21:34:55 -0600

    Refactored String#abbrev ==> String#cabbrev. Added String#labbrev; String#rabbrev.

--- lib/mixins/StringFormatting.rb
+++ lib/mixins/StringFormatting.rb
@@ -4,18 +4,44 @@
 # Copyright:: 2009
 # License::   GPL
 #
+# Adds String#cabbrev for abbreviating strings
+#
 
 class String
 
   # gives an abbreviated form of a string, showing some of the beginning
   # and some of the end.
   # 
-  #  "the quick brown dog".abbrev 12  # => "the q... dog"
-  def abbrev(len)
+  #  "the quick brown dog".cabbrev 12  # => "the q... dog"
+  def cabbrev(len)
     if length <= len
       self
     else
       self[0..(len/2-2).floor] + "..." + self[(length - len/2+2) .. (length)]
+    end
+  end
+
+  # gives an abbreviated form of the string, showing as much of the beginning
+  # as possible
+  #
+  #  "the quick brown dog".labbrev 12  # => "the quick ..."
+  def labbrev(len)
+    if length <= len
+      self
+    else
+      self[0..(len-3)] + '...'
+    end
+  end
+
+  # gives an abbreviated form of the string, showing as much of the end as
+  # possible
+  #
+  #  "the quick brown dog".rabbrev 12  # => "...brown dog"
+  def rabbrev(len)
+    if length <= len
+      self
+    else
+      '...' + self[(length - (len-3)) .. length]
     end
   end
 end
