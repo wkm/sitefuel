26e52c1085024c4623103e31afb9ca9a960d8bce
wkm <wkm@zanoccio.com>
2009-11-26 08:27:38 -0600

    Added a smattering of color to the ts_all test suite

--- test/ts_all.rb
+++ test/ts_all.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby -w
+#!/usr/bin/ruby -w -rubygems
 
 #
 # File::      ts_all.rb
@@ -9,17 +9,25 @@
 # The entirety test suite. Runs all tests.
 #
 
-require 'test/unit' 
+require 'test/unit'
+require 'term/ansicolor'
+include Term::ANSIColor
+
+$Divider = '='*60
 
 # programmatically load all test files in this directory
 testfiles = Dir[File.join(File.dirname(__FILE__), "test_*.rb")];
 
-puts '='*60
-puts 'Loaded:'
+puts $Divider
+puts bold('Found:')
 puts testfiles.join("\n")
-puts '='*60
+
+puts $Divider
+puts bold('Loading:')
 
 testfiles.each do |testfile|
   require testfile
 end
 
+puts $Divider
+puts bold('Results:')
