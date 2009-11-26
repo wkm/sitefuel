51492fc199e36adbc8f75d88cb6bf3e7fd2c0c4d
wkm <wkm@zanoccio.com>
2009-11-26 17:04:54 -0600

    Added the number of LOC without test files.

--- test/loc.sh
+++ test/loc.sh
@@ -14,3 +14,4 @@ echo '------------------------------------------------'
 echo '  everything                 :' $(cat $(find .. -name '*.rb') | wc -l)
 echo '  w/o whitespace             :' $(cat $(find .. -name '*.rb') | grep -v -e "^[ \t\n]*$" | wc -l)
 echo '  w/o whitespace and comments:' $(cat $(find .. -name '*.rb') | grep -v -e "^[ \t\n]*$" -e "^[ \t\n]*#.*$" | wc -l)
+echo '  w/o test-files:            :' $(cat $(find .. -name '*.rb' | grep -v 'test/') | grep -v -e "^[ \t\n]*$" -e "^[ \t\n]*#.*$" | wc -l)
