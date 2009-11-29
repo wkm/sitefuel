2b45b107d3f37ed134075babf75d9f651bd4362e
wkm <wkm@zanoccio.com>
2009-11-29 15:03:20 -0600

    slight reformat of the minification tests; added a slightly more involved CSS test.

--- test/test_CSSProcessor.rb
+++ test/test_CSSProcessor.rb
@@ -31,19 +31,39 @@ class TestCSSProcessor < Test::Unit::TestCase
     assert_equal "CSS", CSSProcessor.processor_name
   end
 
+  # note that minification is based on CSSMin, so it's not too heavily tested
   def test_minify
     # test: whitespace removal; hexcode compaction; comment stripping
-    assert_equal "body{color:#000;}", CSSProcessor.filter_string(
-      :minify,
-      <<-END
-        body {
-          /* comments should be stripped */
-          color: #000000;
-        }
-      END
+    assert_equal(
+      "body{color:#000;}",
+      CSSProcessor.filter_string(
+        :minify,
+        <<-END
+          body {
+            /* comments should be stripped */
+            color: #000000;
+          }
+        END
+      )
     )
 
-    # TODO need many more unit tests here; since it's based on CSSMin we might
-    # just inherit those.
+    assert_equal(
+      "h1 .header{color:white;margin:0;font-family:\"lucida grande\",lucida,verdana,sans-serif;}a:hover{text-decoration:none;}",
+      CSSProcessor.filter_string(
+        :minify,
+        <<-END
+          h1 .header
+          {
+            color: white;
+            margin: 0 0 0 0;
+            font-family: "lucida grande", lucida, verdana, sans-serif;
+          }
+
+          a:hover {
+            text-decoration: none;
+          }
+        END
+      )
+    )
   end
 end
