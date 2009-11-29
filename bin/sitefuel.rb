9894fee32636e055fdc55ef303079527cb95df4c
wkm <wkm@zanoccio.com>
2009-11-29 14:45:53 -0600

    Use rdoc/usage to generate the primary help message content; moved most of the help output to the header comment.

--- bin/sitefuel.rb
+++ bin/sitefuel.rb
@@ -5,23 +5,32 @@
 # A lightweight ruby framework for processing and deploying websites, either
 # from
 #
-# === Introduction
+# == Usage
+#
+#  sitefuel deploy|process SOURCE [OPTIONS]
+#
+# == Introduction
 # sitefuel is a lightweight ruby framework for deploying websites directly from
 # version control. sitefuel includes support for compressing HTML and CSS as
 # well as optimizing PNG graphics. Support is planned for SASS; compressing
 # JavaScript; automatically creating sprites; and supporting more image formats.
 # (and more!)
+# 
 # TODO: add more details
 #
-# === Examples
+# * More information: http://sitefuel.org
+# * Getting started: http://sitefuel.org/getstarted
+# * Documentation: http://sitefuel.org/documentation
+#
+# == Examples
 # Process an already deployed site:
-# <pre>sitefuel process /var/www/</pre>
+#  sitefuel process /var/www/
 #
 # Deploy a site from SVN:
-# <pre>sitefuel deploy svn+ssh://sitefuel.org/svn/tags/21 /var/www/</pre>
+#  sitefuel deploy svn+ssh://sitefuel.org/svn/tags/21 /var/www/
 #
 # Specify a non-default deployment file:
-# <pre>sitefuel process /var/www/ -c customdeployment.yml</pre>
+#  sitefuel process /var/www/ -c customdeployment.yml
 #
 #
 # == Author
@@ -30,6 +39,7 @@
 # == Copyright
 # Copyright (c) 2009 Zanoccio.
 #
+# == License
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
@@ -49,6 +59,7 @@
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 
 require 'rubygems'
+require 'rdoc/usage'
 require 'term/ansicolor'
 
 require 'SiteFuelRuntime'
@@ -67,17 +78,9 @@ def parse_command_line(runtime)
 
   #### BUILD THE OPTIONS PARSER
   opts = OptionParser.new
-  opts.banner =  bold("SiteFuel #{$SiteFuelVersionText}\n")
-  opts.banner << 'A ruby framework for deploying sites from version control.'
-  opts.separator ''
-  opts.separator bold('Usage: sitefuel  COMMAND  ARGUMENTS  [OPTIONS]')
-  opts.separator ''
-  opts.separator bold('Commands:')
-  opts.separator "    deploy\tDeploy a site using sitefuel."
-  opts.separator "    process\tModify an existing deployment in-place."
-  opts.separator "    help\tShow this message."
-  opts.separator ''
-  opts.separator bold('Common options:')
+
+  # the banner text comes from the header comment for sitefuel.rb
+
   opts.on('-oARG', '-o=ARG', '-o PLACE', '--output=ARG', '--output PLACE', String,
           'Where to put a deployed site') do |out|
     runtime.deploy_from = out
@@ -85,19 +88,15 @@ def parse_command_line(runtime)
   opts.on('-v', '--version', 'Gives the version of sitefuel') {|| puts 'sitefuel ' + $SiteFuelVersion.join('.')}
   opts.on('--[no-]verbose', 'Cause sitefuel to be verbose, listing actions as they are preformed') {|| puts 'cause sitefuel to be verbose listing actions as they are preformed' }
   opts.on('-h', '--help', '-?', '--about', 'Gives help for a specific command.') do
+    RDoc::usage_no_exit('Synopsis', 'Usage', 'Introduction')
     puts opts
+    RDoc::usage_no_exit('Examples', 'Author', 'Copyright', 'License')
     exit
   end
 
   opts.separator ''
   opts.separator 'Specific options for a command can be seen with: '
   opts.separator '    COMMAND --help'
-
-
-  opts.separator ''
-  opts.separator 'More information: http://sitefuel.org'
-  opts.separator 'Getting started: http://sitefuel.org/getstarted'
-  opts.separator 'Documentation: http://sitefuel.org/documentation'
 
 
   #### ATTEMPT TO PARSE THE COMMAND LINE
