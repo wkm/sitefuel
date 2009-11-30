52ad8241d61691b31ee6c5076e8e4309f9ec3dff
wkm <wkm@zanoccio.com>
2009-11-29 21:35:50 -0600

    Added a little script to run each test case individually.

---
+++ test/run_tests_individually.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# File::      run_tests_individually.sh
+# Author::    wkm
+# Copyright:: 2009
+# License::   GPL
+#
+# Little bash script to run each test file indivudally. Ocasionally there are
+# load path issues that running the tests all at once hides.
+#
+
+find . -name 'test_*.rb'  -exec echo \; -exec echo '-----------------------' \; -exec echo {} \; -exec ruby {} \;
