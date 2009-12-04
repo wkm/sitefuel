eed532efb3ee8a189c56a7a50b705f9e5832aecd
wkm <wkm@zanoccio.com>
2009-12-04 16:22:48 -0600

    Started hooking into the AbstractExternalProgramProcessor.

--- lib/sitefuel/processors/PNGProcessor.rb
+++ lib/sitefuel/processors/PNGProcessor.rb
@@ -10,21 +10,58 @@
 module SiteFuel
   module Processor
 
-    require 'sitefuel/processors/AbstractProcessor'
+    require 'sitefuel/processors/AbstractExternalProgramProcessor'
 
     # processor for handling Portable Network Graphics images
     # currently operates as a lightweight wrapper around 'pngcrush'
-    class PNGProcessor < AbstractProcessor
+    class PNGProcessor < AbstractExternalProgramProcessor
       def self.file_patterns
         ['.png']
       end
 
-      def self.default_filterset
-        :crush
+      #
+      # External Program Handling
+      #
+
+      # gives the name of the pngcrush binary
+      def self.program_binary
+        'pngcrush'
       end
 
-      def self.filterset_crush
+      def self.program_version_option
+        '-version'
+      end
+
+      # we've only tested 1.5.10; but we're not using pngcrush
+      # in any special way
+      def self.appropriate_program_versions
+        "> 1.5.0"
+      end
+
+
+      #
+      # FILTERS AND FILTERSETS
+      #
+
+      def self.default_filterset
+        :quick
+      end
+
+      def self.filterset_quick
         []
+      end
+
+      def self.filterset_max
+        [:brute]
+      end
+
+
+      #
+      # FILTERS
+      #
+      
+      def filter_brute
+        add_option('--brute')
       end
     end
 
