674d2a5b4749f98b2448572dfde815b5200be38e
wkm <wkm@zanoccio.com>
2009-12-04 19:18:40 -0600

    Added stubs for git and pngcrush wrappers.

---
+++ lib/sitefuel/external/GIT.rb
@@ -0,0 +1,9 @@
+#
+# File::      GIT.rb
+# Author::    wkm
+# Copyright:: 2009
+# License::   GPL
+#
+# Wrapper around the git version control system.
+#
+#
---
+++ lib/sitefuel/external/PNGCrush.rb
@@ -0,0 +1,9 @@
+#
+# File::      PNGCrush.rb
+# Author::    wkm
+# Copyright:: 2009
+# License::   GPL
+#
+# Wrapper around the pngcrush program.
+#
+#
