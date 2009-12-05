f4352b19f065f9037c843721504ca083d6243477
wkm <wkm@zanoccio.com>
2009-12-04 21:19:48 -0600

    Fleshing out ExternalProgram's finding of programs and scripts; version tracking; etc. Started working on some unit tests.

--- lib/sitefuel/external/ExternalProgram.rb
+++ lib/sitefuel/external/ExternalProgram.rb
@@ -10,61 +10,111 @@
 #
 
 module SiteFuel
+  module External
 
-  class ExternalProgram
-
-    # classes which implement ExternalProgram need to define
-    # a self.program_binary class
-
-    # gives the name of the external program
-    def self.program_name
-      self.program_binary
-    end
-
-    # gives a list of compatible versions
-    def self.compatible_versions
-      '> 0.0.0.'
-    end
-
-    # gives true if a binary with a compatible version exists
-    def self.compatible_version?
-
-    end
-
-    # gives true if a given version number is compatible
-    def self.compatible_version_number?(versionnumber)
-
-    end
-
-    # given the output of a program gives the version number or nil
-    # if not available
-    def self.get_program_version(version_output)
-      versions = version_output.scan(/\d+\.\d+(\.\d+)/)
-      if versions.length == 0
-        return nil
-      else
-        return versions[0]
+    # raised when an external program can't be found
+    class ProgramNotFound < StandardError
+      attr_reader :program_name
+      def initialize(program_name)
+        @program_name = program_name
       end
     end
 
-    # gives the version of the program
-    def self.option_version
-      '--version'
+    # lightweight abstraction around a program external to Ruby. The class
+    # is designed to make it easy to use an external program in a batch
+    # fashion. Note that the abstraction does not well support interacting
+    # back and forth with external programs.
+    class ExternalProgram
+
+      @@program_binary = {}
+
+      # classes which implement ExternalProgram need to define
+      # a self.program_name method.
+
+      # gives the location of the external program; uses the =which=
+      # unix command
+      def self.program_binary
+
+        # give the cached version if possible
+        cached = @@program_binary[self]
+        return cached if cached
+
+        # otherwise try to find it:
+        binary = capture_output("which", program_name)
+        if binary.empty?
+          raise ProgramNotFound.new(program_name)
+        else
+          # ensure the binary is resolved with respect to the root path
+          binary = File.expand_path(binary, capture_output('pwd'))
+#          binary = '\"%s\"' % binary
+          @@program_binary[self] = binary
+          binary
+        end
+      end
+
+      # Similar to Kernel#exec, but returns a string of the output
+      def self.capture_output(command, *args)
+        cli = command + ' ' + args.join(' ')
+        #File.read(cli).chop
+        IO.popen(cli, 'r').read.chop
+      end
+
+      # gives true if the program can be found.
+      def self.program_found?
+        program_binary
+      rescue ProgramNotFound
+        false
+      else
+        true
+      end
+
+      # gives a list of compatible versions
+      def self.compatible_versions
+        '> 0.0.0.'
+      end
+
+      # gives true if a binary with a compatible version exists
+      def self.compatible_version?
+
+      end
+
+      # gives true if a given version number is compatible
+      def self.compatible_version_number?(versionnumber)
+
+      end
+
+      # given the output of a program gives the version number or nil
+      # if not available
+      def self.extract_program_version(version_output)
+        version_output[/(\d+\.\d+(\.\d+))/]
+      end
+
+      # gives the version of the program
+      def self.option_version
+        '--version'
+      end
+
+      # gets the version of a program
+      def self.program_version
+        extract_program_version(capture_output(program_binary, option_version))
+      rescue ProgramNotFound
+        return nil
+      end
+
+      # describes options for the program
+      def self.options
+        []
+      end
+
+
+
+      #
+      # INSTANCE METHODS
+      #
+      def add_option()
+
+      end
+
     end
-
-    # describes options for the program
-    def self.options
-      []
-    end
-
-
-
-    #
-    # INSTANCE METHODS
-    #
-    def add_option()
-      
-    end
-
   end
 end
--- test/test_ExternalProgram.rb
+++ test/test_ExternalProgram.rb
@@ -15,10 +15,40 @@ require 'sitefuel/external/ExternalProgram'
 
 include SiteFuel
 
+# a wrapper around a generic program
+class BashProgram < External::ExternalProgram
+  def self.program_name
+    'bash'
+  end
+end
+
+# a wrapper around a program which hopefully doesn't exist ;)
+class BadProgram < External::ExternalProgram
+  def self.program_name
+    'foo183127cowsaydog'
+  end
+end
+
+class TestProgramA < External::ExternalProgram
+  def self.program_name
+    './test_programs/versioning.rb'
+  end
+end
+
 class TestExternalProgram < Test::Unit::TestCase
 
   def test_program_finding
-    
+    assert BashProgram.program_found?
+    assert_equal false, BadProgram.program_found?
+
+    assert TestProgramA.program_found?, 'Couldn\'t find test program "versioning.rb"'
+  end
+
+  def test_getting_program_version
+
+    BashProgram.program_version
+    TestProgramA.program_version
+
   end
   
 end
