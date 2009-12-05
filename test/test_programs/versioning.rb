3b2739caec03261af2f7da261de7a3a537a4ab7b
wkm <wkm@zanoccio.com>
2009-12-04 21:35:10 -0600

    Added a few different versions.

--- test/test_programs/versioning.rb
+++ test/test_programs/versioning.rb
@@ -7,3 +7,20 @@
 #
 # Simple program for testing various versioning controls
 #
+
+
+if not $*.empty?
+  case $*[0]
+    when '--version'
+    puts '0.1.2'
+
+    when '--version-2'
+    puts '0.1.2asdad'
+
+    when '--version-3'
+    puts '0.2'
+
+    when '--version-4'
+    puts 'test-program v.0.1.2'
+  end
+end 
