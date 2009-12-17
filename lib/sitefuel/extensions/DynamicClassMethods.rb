1249dadd165ec87bd2af31ec3aed31d059ed493a
wkm <wkm@zanoccio.com>
2009-12-17 17:37:02 -0600

    Finally found out how to properly dynamically define class methods.

---
+++ lib/sitefuel/extensions/DynamicClassMethods.rb
@@ -0,0 +1,19 @@
+#
+# File::      DynamicClassMethods.rb
+# Author::    wkm
+# Copyright:: 2009
+# License::   GPL
+#
+# Adds a #meta_def function to Object so we can programmatically define class
+# methods.
+#
+
+class Object
+
+  # defines a class method
+  # based on: http://blog.jayfields.com/2007/10/ruby-defining-class-methods.html
+  # and: http://whytheluckystiff.net/articles/seeingMetaclassesClearly.html
+  def define_class_method name, &blk
+    (class << self; self; end).instance_eval { define_method name, &blk}
+  end
+end
