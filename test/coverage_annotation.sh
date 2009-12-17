0cfbf464036acb1eb266bbe6d5ac2bbd2aa814fa
wkm <wkm@zanoccio.com>
2009-12-17 13:39:56 -0600

    Added a simple modification of the coverage.sh script to generate annotated source files.

---
+++ test/coverage_annotation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# File::      coverage_annotation.sh
+# Author::    wkm
+# Copyright:: 2009
+# License::   GPL
+#
+# Bash script to test coverage of the unit test suite and generate annotated
+# HTML for the source code which isn't completely covered.
+#
+
+rcov --comments --only-uncovered --sort coverage -i 'sitefuel/' -x '.*' -T test_*.rb
+
