d617b3a401abe41a6190823fd31c388b28160dff
wkm <wkm@zanoccio.com>
2009-12-17 22:05:59 -0600

    Working on adding support for deployment.yml files.

--- lib/sitefuel/Configuration.rb
+++ lib/sitefuel/Configuration.rb
@@ -8,17 +8,26 @@
 #
 
 module SiteFuel
+  require 'sitefuel/SiteFuelLogger'
   require 'yaml'
 
   # exception which represents that a configuration could not be found
   class Configuration
 
+    include SiteFuel::Logging
+
     # given a directory path will attempt to location a configuration file
     # and load it, returning a SiteFuel::Configuration class
-    def self.load(path)
-      unless File.exist?(path)
+    def self.load(path, filename = 'deployment.yml')
+      files = Dir["**/"+filename]
 
+      if files.length < 0
+        warn('No deployment configuration file found. Using defaults.')
+        return
       end
+
+      config_file = files.
+
 
       yamlconfig = YAML::load_file(configfile)
       
