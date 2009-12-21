1a718563ece8a3765b9098131dbb5d717492a2ac
wkm <wkm@zanoccio.com>
2009-12-20 19:52:52 -0600

    Simple SVN abstraction based on GIT

---
+++ lib/sitefuel/external/SVN.rb
@@ -0,0 +1,41 @@
+#
+# File::      SVN.rb
+# Author::    wkm
+# Copyright:: 2009, Zanoccio LLC.
+# License::   GPL version 2.0
+#
+# Wrapper around the SVN version control system.
+#
+
+module SiteFuel
+  module External
+
+    require 'sitefuel/external/AbstractExternalProgram'
+
+    class SVN < AbstractExternalProgram
+
+      def self.program_name
+        'svn'
+      end
+
+      # again, earlier versions would probably work
+      def self.compatible_versions
+        '> 1.2'
+      end
+      
+
+      option :source, '${source}'
+      option :export, 'export'
+      option :revision, '-r ${value}'
+      option :output, '${value}'
+
+      def self.export(source, output, revision='HEAD')
+        execute :export,
+                :revision, revision,
+                :source, source,
+                :output, output
+      end
+
+    end
+  end
+end
