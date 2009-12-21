ede216749ec7da83e89bee0ffb40f82d37545f5d
wkm <wkm@zanoccio.com>
2009-12-21 15:13:42 -0600

    Started to work on a class configuration abstraction.

---
+++ lib/sitefuel/processors/Configurable.rb
@@ -0,0 +1,8 @@
+#
+# File::      Configurable.rb
+# Author::    wkm
+# Copyright:: 2009, Zanoccio LLC.
+# License::   GPL version 2.0 (see LICENSE.rb)
+#
+# Implements a general abstraction for configuring a class instance
+#
