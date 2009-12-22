2911cd04cdcd0a910f09c1d2da10d3230fe4e21a
wkm <wkm@zanoccio.com>
2009-12-22 14:02:09 -0600

    Work on the config code.

--- lib/sitefuel/Configuration.rb
+++ lib/sitefuel/Configuration.rb
@@ -16,28 +16,56 @@ module SiteFuel
 
     include SiteFuel::Logging
 
+    DEFAULT_CONFIGURATION_FILE = 'deployment.yml'
+
     # given a directory path will attempt to location a configuration file
-    # and load it, returning a SiteFuel::Configuration class
-    def self.find_configuration(path, filename = 'deployment.yml')
+    def self.find_configuration(path, filename = DEFAULT_CONFIGURATION_FILE)
       files = Dir["**/"+filename]
 
       if files.length < 0
-        warn('No deployment configuration file found. Using defaults.')
-        return
+        return nil
+      elsif files.length > 1
+        warn("Multiple  possible configuration files found: #{files.join(', ')}; using #{files.first}")
+        return files.first
+      else
+        return files.first
       end
-
-
-
-      config_file = files.
-
-
-      yamlconfig = YAML::load_file(configfile)
-      
-      Configuration.new(yamlconfig)
     end
 
+
+    # given a directory path will attempt to find and load a
+    # configuration file
+    def self.load_configuration(runtime, path, filename = DEFAULT_CONFIGURATION_FILE)
+      configuration_file = find_configuration(path, filename)
+
+      if configuration_file == nil
+        warn('No deployment configuration file found. Using defaults.')
+        return Configuration.new(runtime, nil)
+      else
+        yaml = YAML::load_file(configuration_file)
+        return Configuration.new(runtime, yaml)
+      end
+    end
+
+    
     # builds a sitefuel configuration from a parsed YAML file
-    def initialize(yamlconfig)
+    def initialize(runtime, yaml_config)
+      @processor_configurations = Hash.new([])
+      @runtime = runtime
+
+      load_yaml_config(yaml_config)
+    end
+
+
+    # loads a yaml configuration
+    def load_yaml_config(yaml_config)
+      
+    end
+
+
+    # gives an array of configuration options for the processor
+    def processor_configuration(processor)
+      @processor_configurations[processor]
     end
   end
 end
